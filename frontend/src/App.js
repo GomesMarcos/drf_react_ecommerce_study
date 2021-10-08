a568de75db95248ccf80a963c600b74012d58214
GomesMarcos <gomes.marcosjf@gmail.com>
2021-10-07 23:08:16 -0300

    WIP HomeScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,15 @@ import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
+import HomeScreen from './screens/HomeScreen'
+
 function App() {
   return (
     <div>
       <Header />
       <main>
         <Container>
-          <h1>Wellcome</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
